a6badc9c6da2fd6bf1c96116b78dafebe0ce209d
Menng <concisesky@gmail.com>
2020-10-20 14:54:12 +0800

    apollo demo init

---
+++ src/main/java/com/aaron/apollo/HelloController.java
@@ -0,0 +1,21 @@
+package com.aaron.apollo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @Value("${hello}")
+    String port;
+
+    @GetMapping("hi")
+    public String hi(String name) {
+        return "hi " + name + " , i am from port:" + port;
+    }
+}
